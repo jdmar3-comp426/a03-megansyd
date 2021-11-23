4a58f34c028abe5cf01bffbe5f78a247c6408ba9
megan nickel <megansyd@live.unc.edu>
2021-11-22 21:35:14 -0500

    fixing identifyVariable function

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -7,7 +7,7 @@
  */
 export function identifyVariable(variable) {
    let type = typeof variable;
-   return "{ type: '" + type + "', value: " + variable + " }";
+   return "{ type: '" + type + "', value: '" + variable + "' }";
 }
 
 
