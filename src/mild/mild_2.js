5dff776eee252f9db92f2b740edb29160f6ebbda
megan nickel <megansyd@live.unc.edu>
2021-11-27 18:58:23 -0500

    part 6 maybe?

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -77,7 +77,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let obj = object;
+   let obj = Object.assign({}, object);
    delete obj[key];
    return obj;
 }
@@ -104,7 +104,7 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-   let obj = object;
+   let obj = Object.assign({}, object);
    for (let i = 0; i < keyList.length; i++) {
       delete obj[keyList[i]];
    }
