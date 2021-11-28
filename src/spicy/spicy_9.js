b0ef9917b4248f9c9d5b9f4fca9d4aa363272f50
megan nickel <megansyd@live.unc.edu>
2021-11-27 19:48:58 -0500

    part 2

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -45,11 +45,9 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-    function innerfunction(a, b){
+    function innerfunction(num2){
         return num1 * num2;
     }
-    let first = num1;
-    let second = 5;
     return innerfunction(num1, num2); 
 };
 
